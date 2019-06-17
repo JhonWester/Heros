a7f9383f07a31677c34ba47c4bedd11b0c17562f
Jhon James <jhonjameshernandez63@gmail.com>
2019-06-17 10:13:26 -0500

    version1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
+// servicios
+import { HeroService } from './components/services/hero.service';
+
+// rutas
+import { AppRoutingModule} from './app.routes';  
+
+// componentes
+import { HomeComponent } from './components/home/home.component';
+ 
+import { HerosComponent } from './components/heros/heros.component';
+import { AboutComponent } from './components/about/about.component';
+ 
+import { from } from 'rxjs';
+import { HeroesComponent } from './components/heroes/heroes.component';
+ 
+ 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+ 
+    HerosComponent,
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    HeroService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
